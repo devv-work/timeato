08c47cc9b7aa85067ae040c77eee2f2993ccb7cc
brianschnee <brian.a.schnee@gmail.com>
2022-09-02 15:29:38 -0400

    Fix: updated timetracker route import

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
-const todoRoutes = require('./routes/todos')
+const timeTrackerRoutes = require('./routes/timetracker')
 
 require('dotenv').config({ path: './config/.env' })
 
