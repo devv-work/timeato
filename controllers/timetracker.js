d86d48fb506e6b0f1d34dbc27d880cd130e2f57e
Brandon-Schefstad <bschefstad@gmail.com>
2022-09-02 16:45:41 -0400

    Feat: Adding methods and paths to TimeTracker controller

--- controllers/timetracker.js
+++ controllers/timetracker.js
@@ -1,5 +1,13 @@
 const TimeTracker = require('../models/TimeTracker')
 
 module.exports = {
+    getTime: (req,res)=>{
 
+    },
+    addTime: (req,res)=>{
+
+    },
+    deleteTime: (req,res)=>{
+
+    },
 }    
--- routes/timetracker.js
+++ routes/timetracker.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const timeTrackerController = require('../controllers/timetracker')
 const { ensureAuth } = require('../middleware/auth')
 
-
+// Method names are arbitary
+router.get('/', timeTrackerController.getTime)
+router.post('/addTime', timeTrackerController.addTime)
+router.delete('/deleteTime', timeTrackerController.deleteTime)
 module.exports = router
