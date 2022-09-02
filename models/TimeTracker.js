26b3ac184d2423358c7c02db58d04012d81f19d9
Brandon-Schefstad <bschefstad@gmail.com>
2022-09-02 13:40:49 -0400

    Fix: Removing password packages and middleware

--- models/TimeTracker.js
+++ models/TimeTracker.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
@@ -6,31 +5,6 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String
 })
-
-
-// Password hash middleware.
- 
- UserSchema.pre('save', function save(next) {
-  const user = this
-  if (!user.isModified('password')) { return next() }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err) }
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) { return next(err) }
-      user.password = hash
-      next()
-    })
-  })
-})
-
-
-// Helper method for validating user's password.
-
-UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch)
-  })
-}
 
 
 module.exports = mongoose.model('User', UserSchema)
