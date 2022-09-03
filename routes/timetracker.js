76ee708c3d789003bd60e45a49b3b643b97712bd
brianschnee <brian.a.schnee@gmail.com>
2022-09-02 22:07:56 -0400

    Fix: updating routes imports

--- routes/timetracker.js
+++ routes/timetracker.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const timeTrackerController = require('../controllers/timetracker')
+const taskController = require('../controllers/task')
 const { ensureAuth } = require('../middleware/auth')
 
 // Method names are arbitary
-router.get('/', ensureAuth, timeTrackerController.getTime)
-router.post('/addTime', ensureAuth, timeTrackerController.addTime)
-router.delete('/deleteTime', ensureAuth, timeTrackerController.deleteTime)
+router.get('/', ensureAuth, taskController.getTime)
+router.post('/addTime', ensureAuth, taskController.addTime)
+router.delete('/deleteTime', ensureAuth, taskController.deleteTime)
 
 //export
 module.exports = router
