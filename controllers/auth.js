03f3e3a46ec71be5c2985c6c27b8a219bf34cdcb
brianschnee <brian.a.schnee@gmail.com>
2022-09-02 22:05:44 -0400

    Fix: changing redirects to /task route

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 
 exports.getLogin = (req, res) => {
   if (req.user) {
-    return res.redirect('/timetracker')
+    return res.redirect('/task')
   }
   res.render('login', {
     title: 'Login'
@@ -31,7 +31,7 @@ exports.postLogin = (req, res, next) => {
     req.logIn(user, (err) => {
       if (err) { return next(err) }
       req.flash('success', { msg: 'Success! You are logged in.' })
-      res.redirect(req.session.returnTo || '/timetracker')
+      res.redirect(req.session.returnTo || '/task')
     })
   })(req, res, next)
 }
@@ -49,7 +49,7 @@ exports.logout = (req, res) => {
 
 exports.getSignup = (req, res) => {
   if (req.user) {
-    return res.redirect('/timetracker')
+    return res.redirect('/task')
   }
   res.render('signup', {
     title: 'Create Account'
@@ -91,7 +91,7 @@ exports.postSignup = (req, res, next) => {
         if (err) {
           return next(err)
         }
-        res.redirect('/timetracker')
+        res.redirect('/task')
       })
     })
   })
