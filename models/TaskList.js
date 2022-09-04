43414a76c10bb226a47db9cd238a8e511d0d98b5
brianschnee <brian.a.schnee@gmail.com>
2022-09-04 00:22:44 -0400

    Fix: adding header for text area and logo

--- models/TaskList.js
+++ models/TaskList.js
@@ -2,24 +2,24 @@ const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
   taskName: {
-      type: String,
-      default: 'study'
+    type: String,
+    default: 'study'
   },
   elapsedTime: {
-      type: Number,
-      min: 0,
-      default: 0,
+    type: Number,
+    min: 0,
+    default: 0,
   },
   totalSessions: {
-      type: Number,
-      min: 0,
-      default: 0,
+    type: Number,
+    min: 0,
+    default: 0,
   }
 })
 
 const TaskListSchema = new mongoose.Schema({
   taskList: {
-      type:[TaskSchema],
+    type: [TaskSchema],
   }
 })
 
