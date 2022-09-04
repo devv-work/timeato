40bf611b6a376bd899a4c4baeca1e5144dfc3ffb
Brandon-Schefstad <bschefstad@gmail.com>
2022-09-03 21:08:01 -0400

    Feat: Logout route works, session is destroyed

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const User = require('../models/User')
 const TaskList = require('../models/TaskList')
 
 exports.getLogin = (req, res) => {
+  console.log(req.user)
   if (req.user) {
     return res.redirect('/task')
   }
@@ -38,12 +39,8 @@ exports.postLogin = (req, res, next) => {
 }
 
 exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log('User has logged out.')
-  })
   req.session.destroy((err) => {
-    if (err) console.log('Error : Failed to destroy the session during logout.', err)
-    req.user = null
+    console.log(req.user)
     res.redirect('/')
   })
 }
--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,6 @@
 module.exports = {
     getIndex: (req,res)=>{
+        console.log(req.user)
         res.render('index.ejs')
     }
 }
