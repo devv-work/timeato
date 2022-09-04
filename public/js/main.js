ae4be979f48d18e124e2a675bc3b2e3c041f1b04
Brandon-Schefstad <bschefstad@gmail.com>
2022-09-04 18:40:21 -0400

    Checkpoint commit for later

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ const timerStartStopBtn = document.querySelector('.timerStartStop');
 timerStartStopBtn.addEventListener('click', handleTimer);
 // Display with 'Minutes Left: _____"
 const timerDisplay = document.querySelector('.timerDisplay');
-// Defines a class for timer, if we want to add methods/properties in mongoDb our schema is 
+// Defines a class for timer, if we want to add methods/properties in mongoDb our schema is
 class Timer {
 	constructor() {
 		(this.startTime = 0), (this.elapsedTime = 0);
@@ -12,37 +12,50 @@ class Timer {
 }
 
 // Creates a timer upon page load
-const testTimer = new Timer(0)
+const testTimer = new Timer(0);
 
-// Grabs the selected time from dropdown, assigns it to startTime in our Timer. 
+// Grabs the selected time from dropdown, assigns it to startTime in our Timer.
 // Updates 'Minutes Left: ___" with appropriate number
-// Calls decrementMinutes
+// Calls startTimer
 function handleTimer() {
+	testTimer.startTime === 0 ? startTimer() : console.log('StopTimer');
 	testTimer.startTime = parseInt(document.querySelector('#timeSelect').value);
-	timerDisplay.innerText = 'Minutes Left: ' + testTimer.startTime;
-	decrementMinutes(testTimer.startTime);
+	// } else {
+	// 	alert('handle stop')
+	// }
 }
-
-// Might pull apart this function into smaller ones, but essentially it checks for the current time remaining, 
-//	if it is >=10 it will append it to the 'Minutes Left:' normally,
-//	  if it is <10 it will add a leading 0 (so it is always two digits like a clock), 
-//		  finally if it is 0 it will alert 'FINISHED' and redeclare testTimer.startTime to stop the loop.
 function decrementMinutes() {
+	testTimer.startTime -= 1;
+	testTimer.elapsedTime += 1;
+}
+function stopTimer() {
+	timerDisplay.innerText = 'Minutes Left: ' + testTimer.startTime;
+	console.log('stopTimer');
+}
+// Might pull apart this function into smaller ones, but essentially it checks for the current time remaining,
+//	if it is >=10 it will append it to the 'Minutes Left:' normally,
+//	  if it is <10 it will add a leading 0 (so it is always two digits like a clock),
+//		  finally if it is 0 it will alert 'FINISHED' and redeclare testTimer.startTime to stop the loop.
+function startTimer() {
+	console.log('startTimer');
+	timerDisplay.innerText = 'Minutes Left: ' + testTimer.startTime;
 	console.log(testTimer);
 	if (testTimer.startTime > 0) {
-		if (testTimer.startTime  >= 10) {
+		if (testTimer.startTime >= 10) {
 			timerDisplay.innerText = 'Minutes Left: ' + testTimer.startTime;
-			testTimer.startTime -= 1;
-			setTimeout(decrementMinutes, 1000);
+			decrementMinutes();
+			// DEV NOTE: Update to 60000 for minute countdown, maybe add feedback to let user know it is working e.g. changing color or something?
+			setTimeout(startTimer, 1000);
 		} else {
 			timerDisplay.innerText = 'Minutes Left: 0' + testTimer.startTime;
-			testTimer.startTime -= 1;
-			setTimeout(decrementMinutes, 10);
+			decrementMinutes();
+			setTimeout(startTimer, 10);
 		}
 	} else {
 		timerDisplay.innerText = 'Minutes Left: 00';
-		alert('FINISHED')
-		testTimer.startTime = parseInt(document.querySelector('#timeSelect').value)
+		alert('FINISHED');
+		testTimer.startTime = parseInt(
+			document.querySelector('#timeSelect').value
+		);
 	}
 }
-
