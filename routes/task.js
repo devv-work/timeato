8e8f8f3615617fcfa32d712d88fa2d8fa17a1601
Brandon-Schefstad <bschefstad@gmail.com>
2022-09-03 20:02:42 -0400

    Fix: Capitalizing again

--- routes/task.js
+++ routes/task.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const taskController = require('../controllers/task')
 const { ensureAuth } = require('../middleware/auth')
 
-// Method names are arbitary.
+// Method names are arbitary
 router.get('/', ensureAuth, taskController.getTime)
 router.post('/addTime', ensureAuth, taskController.addTime)
 router.delete('/deleteTime', ensureAuth, taskController.deleteTime)
